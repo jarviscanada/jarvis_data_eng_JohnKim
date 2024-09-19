2598deadc8e161ca4ad82e255db61035ef02dc94
peanutKim <bigdpg@DESKTOP-L9AMOD1>
2024-09-18 22:00:11 -0400

    docs: improve comments for better clarity and description

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -6,10 +6,9 @@ db_username=$2
 db_password=$3
 
 # Start docker
-# Make sure you understand the double pipe operator
 sudo systemctl status docker --no-pager || sudo systemctl start docker
 
-# Check container status (try the following cmds on terminal)
+# Check container status
 docker container inspect jrvs-psql
 container_status=$?
 
@@ -33,12 +32,11 @@ case $cmd in
 	docker volume create pgdata
   # Start the container
 	docker run --name jrvs-psql -e POSTGRES_USER="$db_username" -e POSTGRES_PASSWORD="$db_password" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
-  # Make sure you understand what's `$?`
 	exit $?
 	;;
 
   start|stop)
-  # Check instance status; exit 1 if container has not been created
+  # exit 1 if container has not been created
   if [ $container_status -ne 0 ]; then
     echo 'Container has not been created'
     exit 1
